0a174571b1b8aaeb977b2e21dd286ba4aad019c6
Halfloaf <alex.halbleib@gmail.com>
2025-09-26 23:04:14 -0400

    Update init.lua with custom terminal

--- init.lua
+++ init.lua
@@ -25,58 +25,197 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- add your plugins here
+		"neovim/nvim-lspconfig",
 		{
 			"mason-org/mason.nvim",
 			opts = {}
-		}
+		},
+		"nvim-mini/mini.pick"
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "slate" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
 
--- install mini pack
--- Put this at the top of 'init.lua'
-local path_package = vim.fn.stdpath('data') .. '/site'
-local mini_path = path_package .. '/pack/deps/start/mini.pick'
-if not vim.loop.fs_stat(mini_path) then
-	vim.cmd('echo "Installing `mini.pick`" | redraw')
-	local clone_cmd = {
-		'git', 'clone', '--filter=blob:none',
-		-- Uncomment next line to use 'stable' branch
-		'--branch', 'stable',
-		'https://github.com/nvim-mini/mini.pick', mini_path
-	}
-	vim.fn.system(clone_cmd)
-	vim.cmd('packadd mini.pack | helptags ALL')
-	vim.cmd('echo "Installed `mini.pack`" | redraw')
-end
+-- theme and transparency
+vim.cmd.colorscheme("slate")
 
 -- basic option settings
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
+
+-- Indentation
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
-vim.opt.wrap = false
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
 vim.opt.smartindent = true
-vim.opt.winborder = 'rounded'
-vim.opt.signcolumn = "yes"
+vim.opt.autoindent = true
+
+-- Search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+-- Visual settings
+vim.opt.winborder = 'rounded'									-- Pop up windows show a rounded border
+vim.opt.signcolumn = "yes"										-- Always show the sign column
+vim.opt.termguicolors = true									-- Enable 24-bit colors
+vim.opt.colorcolumn = "100"										-- Show column at 100 characters
+vim.opt.showmatch = true											-- Highlight matching brackets
+vim.opt.matchtime = 2
+vim.opt.completeopt = "menuone,noinsert,noselect,popup"
+
+-- File handling
+vim.opt.autoread = true
+
+-- Behavior settings
+vim.opt.hidden = true                         -- allow hidden buffers
+vim.opt.path:append("**")                     -- include subdirectories in search
 
 -- keymappings 
-vim.keymap.set('n', '<leader>w', ':write<cr>')
-vim.keymap.set('n', '<leader>s', ':write<cr> :so<cr>')
-vim.keymap.set('n', '<leader>m', ':Mason<cr>')
-vim.keymap.set('n', '<leader>q', ':quit<cr>')
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-vim.keymap.set('n', '<leader>k', vim.lsp.buf.definition)
+vim.keymap.set('n', '<leader>w', ':write<cr>', { desc = "Fast Write" })
+vim.keymap.set('n', '<leader>m', ':Mason<cr>', { desc = "Open Mason" })
+vim.keymap.set('n', '<leader>q', ':quit<cr>', { desc = "Fast Quit" })
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { desc = "Format current file" })
+vim.keymap.set('n', '<leader>k', vim.lsp.buf.definition, { desc = "Go to Definition" })
+vim.keymap.set('n', '<leader>rc', ':e ~/.config/nvim/init.lua<cr>', { desc = "Open nvim config" })
+vim.keymap.set('n', '<leader>d', '"_d', { desc = "Delete without yanking" })
+vim.keymap.set('n', '<leader>y', '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set('n', '<leader>p', '"+p', { desc = "Put from system clipboard" })
 
+-- buffer navigation
+vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { desc = "next buffer" })
+vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { desc = "previous buffer" })
+
+-- Better window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+-- Better indenting in visual mode
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+-- Quick file navigation
+vim.keymap.set("n", "<leader>e", ":Explore<CR>", { desc = "Open file explorer" })
+vim.keymap.set("n", "<leader>ff", ":find ", { desc = "Find file" })
+
+
+-- ================================================================================================
+-- 
+-- FLOATING TERMINAL
+--
+-- ================================================================================================
+
+-- terminal
+local terminal_state = {
+  buf = nil,
+  vin = nil,
+  is_open = false
+}
+
+local function FloatingTerminal()
+  -- if terminal is already open, close it (toggle behavior)
+  if terminal_state.is_open and vim.api.nvim_win_is_valid(terminal_state.win) then
+    vim.api.nvim_win_close(terminal_state.win, false)
+    terminal_state.is_open = false
+    return
+  end
+
+  -- create buffer if it doesn't exist or is invalid
+  if not terminal_state.buf or not vim.api.nvim_buf_is_valid(terminal_state.buf) then
+    terminal_state.buf = vim.api.nvim_create_buf(false, true)
+    -- Set buffer options for better terminal experience
+    vim.api.nvim_buf_set_option(terminal_state.buf, 'bufhidden', 'hide')
+  end
+  
+  -- Calculate window dimensions
+  local width = math.floor(vim.o.columns * 0.8)
+  local height = math.floor(vim.o.lines * 0.8)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+
+  -- Create the floating window
+  terminal_state.win = vim.api.nvim_open_win(terminal_state.buf, true, {
+    relative = 'editor',
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = 'minimal',
+    border = 'rounded',
+  })
+
+  -- Set transparency for the floating window
+  vim.api.nvim_win_set_option(terminal_state.win, 'winblend', 0)
+
+  -- Set up transparent background for the window
+  vim.api.nvim_win_set_option(terminal_state.win, 'winhighlight',
+  'Normal:FloatingTermNormal,FloatBorder:FloatingTermBorder')
+
+  -- Define highlight groups for transparency
+  vim.api.nvim_set_hl(0, "FloatingTermNormal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "FloatingTermBorder", { bg = "none" })
+
+  -- Start terminal if not already running
+  local has_terminal = false
+  local lines = vim.api.nvim_buf_get_lines(terminal_state.buf, 0, -1, false)
+  for _, line in ipairs(lines) do
+    if line ~= "" then
+      has_terminal = true
+      break
+    end
+  end
+
+  if not has_terminal then
+    vim.fn.termopen(os.getenv("SHELL"))
+  end
+
+  terminal_state.is_open = true
+  vim.cmd("startinsert")
+
+  -- set up auto-close on buffer leave
+  vim.api.nvim_create_autocmd("BufLeave", {
+    buffer = terminal_state.buf,
+    callback = function()
+      if terminal_state.is_open and vim.api.nvim_win_is_valid(terminal_state.win) then
+        vim.api.nvim_win_close(terminal_state.win, false)
+        terminal_state.is_open = false
+      end
+    end,
+    once = true
+  })
+end
+
+-- Function to explicitly close the terminal
+local function CloseFloatingTerminal()
+  if terminal_state.is_open and vim.api.nvim_win_is_valid(terminal_state.win) then
+    vim.api.nvim_win_close(terminal_state.win, false)
+    temrinal_state.is_open = false
+  end
+end
+
+-- Key mappings
+vim.keymap.set("n", "<leader>t", FloatingTerminal, { noremap = true, silent = true, desc = "toggle floating terminal" })
+vim.keymap.set("t", "<Esc>", function()
+  if terminal_state.is_open then
+    vim.api.nvim_win_close(terminal_state.win, false)
+    terminal_state.is_open = false
+  end
+end, { noremap = true, silent = true, desc = "Close floating terminal from terminal mode" })
+
+-- ================================================================================================
+--
 -- lsp stuff
--- my mason installs ruff and basedpyright
-require('mason').setup()
-
-
+--
+-- ================================================================================================
 vim.api.nvim_create_autocmd('LspAttach', {
 	group = vim.api.nvim_create_augroup('my.lsp', {}),
 	callback = function(args)
@@ -96,4 +235,3 @@ vim.lsp.enable({
 	'basedpyright'
 })
 
-vim.cmd("set completeopt+=noselect,menuone,popup")
